4a196bc9ead4564beee0b692cc74087a37d17967
GustavoSchattner <43237497+GustavoSchattner@users.noreply.github.com>
2022-05-24 17:26:28 -0300

    Feat: Criação de Api

---
+++ pages/api/bancoDeQuestoes.ts
@@ -0,0 +1,34 @@
+import QuestaoModel from "../../model/questao";
+import RespostaModel from "../../model/resposta";
+
+const questoes: QuestaoModel[] = [
+    new QuestaoModel(306, 'Qual bicho é o chupacu ?', [
+        RespostaModel.errada('Abelha'),
+        RespostaModel.errada('Ovelha'),
+        RespostaModel.errada('Beterraba'),
+        RespostaModel.certa('Sua Tia'),
+    ]),
+
+    new QuestaoModel(202, 'No Pai de Família, qual a profissão do Urso ?', [
+        RespostaModel.errada('Delicia'),
+        RespostaModel.errada('Suco de Laranja'),
+        RespostaModel.errada('Bolo de Murango'),
+        RespostaModel.certa('Mecânico'),
+    ]),
+
+    new QuestaoModel(203, 'Qual o coletivo de Capivara?', [
+        RespostaModel.errada('Capivaras'),
+        RespostaModel.errada('Capivarais'),
+        RespostaModel.errada('Capimuitas'),
+        RespostaModel.certa('Capivarias'),
+    ]),
+
+    new QuestaoModel(204, 'Qual o coletivo de lápis ?', [
+        RespostaModel.errada('Capivarias'),
+        RespostaModel.errada('Lapises'),
+        RespostaModel.errada('Lapes'),
+        RespostaModel.certa('Não tem porquê lápis já coletivo.'),
+    ]),
+]
+
+export default questoes
